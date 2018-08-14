de97cf5b385a7b767ce1cfda5bb9c9c4ff461f1f
Jordi Coscolla <jcoscol@thoughtworks.com>
2018-08-14 08:42:35 +0200

    ADD eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+	"jest": true,
+	"node": true,
+	"es6": true
+    },
+};
--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-'use strict';
+
 
 const Hapi = require('hapi');
 
 const server = Hapi.server({
-    port: 3000,
-    host: '0.0.0.0'
+  port: 3000,
+  host: '0.0.0.0',
 });
 
 const init = async () => {
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 server.route({
   method: 'GET',
   path: '/',
-  handler: (request, h) => {
-      return 'Hello, world!';
-  }
+  handler: () => 'Hello, world!',
 });
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
